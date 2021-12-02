1bb38f1d2c392148089bdde36b2beee42402d05e
cesar henrique <cesar_silk321@hotmail.com>
2021-12-02 16:50:25 -0300

    Ajustes no eslint e tslint

---
+++ src/app/shared/directives/debug.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DebugDirective } from './debug.directive';
+
+describe('DebugDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DebugDirective();
+    expect(directive).toBeTruthy();
+  });
+});
