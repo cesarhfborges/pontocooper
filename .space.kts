f17439735a5b8e08e83cbaf47d2e24f98d472dd0
cesar henrique <cesar_silk321@hotmail.com>
2022-05-05 11:47:45 -0300

    nova versao

--- .space.kts
+++ .space.kts
@@ -4,7 +4,7 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Run npm steps") {
+job("Build apk") {
     startOn {
         gitPush {
             enabled = true
@@ -12,45 +12,29 @@ job("Run npm steps") {
         }
     }
 
-    container(displayName = "Run publish script", image = "node:14-alpine") {
-        env["REGISTRY"] = "https://npm.pkg.jetbrains.space/mycompany/p/projectkey/mynpm"
+    container(displayName = "ionic-capacitor-image", image = "robingenz/ionic-capacitor github.com/robingenz/docker-ionic-capacitor") {
+        // env["GOOGLE_SA_KEY"] = Secrets("google_sa_key")
+        // env["KEY_STORE"] = Secrets("key_store")
+        // env["KEY_STORE_PASSWORD"] = Secrets("key_store_password")
+        // env["KEY_PASSWORD"] = Secrets("key_password")
+        // env["KEY_ALIAS"] = Params("key_alias")
+        // env["REGISTRY"] = "https://npm.pkg.jetbrains.space/mycompany/p/projectkey/mynpm"
+
+        // echo Get private signing key...
+        // echo ${'$'}KEY_STORE > upload_key.hex
+        // xxd -plain -revert upload_key.hex  upload_key.jks
+        // echo Get Google service account key...
+        // echo ${'$'}GOOGLE_SA_KEY > google_sa_key.hex
+        // xxd -plain -revert google_sa_key.hex  google_sa_key.json
+        // echo Build and pulbish AAB...
+        // ./gradlew publishBundle
+
         shellScript {
-            interpreter = "/bin/sh"
             content = """
-            	echo install ionic/cli
+                echo install ionic/cli
                 npm install -g @ionic/cli@6.18.1
                 echo Install npm dependencies...
                 npm ci
-            """
-        }
-    }
-}
-
-job("Build and publish bundle to internal track") {
-    startOn {
-        gitPush {
-            enabled = true
-            repository = "master"
-        }
-    }
-
-    container("Build and publish", "mycompany.registry.jetbrains.space/p/projectkey/mydocker/automation-android:1.0.5") {
-        env["GOOGLE_SA_KEY"] = Secrets("google_sa_key")
-        env["KEY_STORE"] = Secrets("key_store")
-        env["KEY_STORE_PASSWORD"] = Secrets("key_store_password")
-        env["KEY_PASSWORD"] = Secrets("key_password")
-        env["KEY_ALIAS"] = Params("key_alias")
-
-        shellScript {
-            content = """
-                echo Get private signing key...
-                echo ${'$'}KEY_STORE > upload_key.hex
-                xxd -plain -revert upload_key.hex  upload_key.jks
-                echo Get Google service account key...
-                echo ${'$'}GOOGLE_SA_KEY > google_sa_key.hex
-                xxd -plain -revert google_sa_key.hex  google_sa_key.json
-                echo Build and pulbish AAB...
-                ./gradlew publishBundle
             """
         }
     }
