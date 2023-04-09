e185f141872fb5200b60c49e97561ea9c32a66ee
Cesar Henrique <cesar_silk321@hotmail.com>
2023-04-09 11:26:44 -0300

    novos dados da migração

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SessionStorageService} from '../core/services/session-storage.service';
+import {Observable, timer} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
+
+  dataAtual: Date = new Date();
+  timer: Observable<number> | undefined;
+
+
   constructor(
     private session: SessionStorageService
   ) {
   }
 
   ngOnInit(): void {
+    this.dataAtual = new Date();
+    this.timer = timer(1000, 1000);
+  }
+
+  ngOnDestroy(): void {
+    this.timer = undefined;
   }
 
   teste() {
