5b8b20900acdcc2f3b61327fdffab4a2a4d91e72
Cesar Borges <cesar_silk321@hotmail.com>
2023-05-07 19:17:20 -0300

    mocks com metodos antigos comentados para refatoramento posterior

--- src/app/shared/mockTesting/MockPlatform.ts
+++ src/app/shared/mockTesting/MockPlatform.ts
@@ -7,29 +7,29 @@ export interface BackButtonEmitter extends Subject<BackButtonEventDetail> {
 
 export default class MockPlatform {
 
-  backButton: BackButtonEmitter;
-  keyboardDidShow: any;
-  keyboardDidHide: Subject<void>;
-  pause: Subject<void>;
-  resume: Subject<void>;
-  resize: Subject<void>;
-  readonly isRTL: boolean;
-  private doc;
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  private _readyPromise;
-  private win;
-
-  constructor() {
-  }
-
-  ready(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  is(type: string): Promise<any> {
-    return Promise.resolve();
-  }
-
-  platforms() {
-  }
+  // backButton: BackButtonEmitter;
+  // keyboardDidShow: any;
+  // keyboardDidHide: Subject<void>;
+  // pause: Subject<void>;
+  // resume: Subject<void>;
+  // resize: Subject<void>;
+  // readonly isRTL: boolean;
+  // private doc;
+  // // eslint-disable-next-line @typescript-eslint/naming-convention
+  // private _readyPromise;
+  // private win;
+  //
+  // constructor() {
+  // }
+  //
+  // ready(): Promise<void> {
+  //   return Promise.resolve();
+  // }
+  //
+  // is(type: string): Promise<any> {
+  //   return Promise.resolve();
+  // }
+  //
+  // platforms() {
+  // }
 }
--- src/app/shared/mockTesting/MockThemeDetection.ts
+++ src/app/shared/mockTesting/MockThemeDetection.ts
@@ -1,19 +1,19 @@
-import {ThemeDetectionResponse} from '@ionic-native/theme-detection';
+// import {ThemeDetectionResponse} from '@ionic-native/theme-detection';
 
 export default class MockThemeDetection {
-  isAvailable(): Promise<ThemeDetectionResponse> {
-    const response: ThemeDetectionResponse = {
-      value: true,
-      message: 'dark'
-    };
-    return Promise.resolve(response);
-  }
-
-  isDarkModeEnabled(): Promise<ThemeDetectionResponse> {
-    const response: ThemeDetectionResponse = {
-      value: true,
-      message: 'dark'
-    };
-    return Promise.resolve(response);
-  }
+  // isAvailable(): Promise<ThemeDetectionResponse> {
+  //   const response: ThemeDetectionResponse = {
+  //     value: true,
+  //     message: 'dark'
+  //   };
+  //   return Promise.resolve(response);
+  // }
+  //
+  // isDarkModeEnabled(): Promise<ThemeDetectionResponse> {
+  //   const response: ThemeDetectionResponse = {
+  //     value: true,
+  //     message: 'dark'
+  //   };
+  //   return Promise.resolve(response);
+  // }
 }
