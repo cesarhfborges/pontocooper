5614637c530f7ac6aa311b2a15252855cd947e6e
Cesar Borges <cesar_silk321@hotmail.com>
2023-05-18 13:44:46 -0300

    teste unitário do preventFocus directive

--- src/app/shared/directives/prevent-focus.directive.ts
+++ src/app/shared/directives/prevent-focus.directive.ts
@@ -7,21 +7,9 @@ export class PreventFocusDirective {
 
   constructor(
     private elRef: ElementRef
-  ) {
-    // const teste: ElementRef = this.elRef.nativeElement;
-    // console.log(teste);
-    // console.log(typeof teste);
-    // const el = this.elRef.nativeElement as HTMLElement;
-    // el.addEventListener('click', ($event) => {
-    //   $event.preventDefault();
-    //   $event.stopImmediatePropagation();
-    //   console.log('clickei');
-    // });
-  }
+  ) {}
 
   @HostListener('click', ['$event']) onClick($event: any) {
-    // const el = this.elRef.nativeElement as HTMLElement;
-    // console.log(el);
     $event.preventDefault();
     $event.stopImmediatePropagation();
     this.recursiveBlur(this.elRef.nativeElement.children);
@@ -29,7 +17,6 @@ export class PreventFocusDirective {
 
   private recursiveBlur(elements: any[]): void {
     for (const element of elements) {
-      // console.log(element);
       if (element.children.length > 0) {
         this.recursiveBlur(element.children);
       } else {
