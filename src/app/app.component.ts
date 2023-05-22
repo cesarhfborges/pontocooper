59aa531c21756f174de477fd4692ea307b64a3e3
Cesar Borges <cesar_silk321@hotmail.com>
2023-05-22 15:01:42 -0300

    ajuste na chamada do metodo para que retorne os dados do mes selecionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Network } from '@capacitor/network';
 import { ConnectionStatus } from '@capacitor/network/dist/esm/definitions';
 import { Router } from '@angular/router';
@@ -6,13 +6,17 @@ import { NavController, Platform } from '@ionic/angular';
 import { ScreenOrientation } from '@capacitor/screen-orientation';
 import { environment } from '../environments/environment';
 import { AppLauncher } from '@capacitor/app-launcher';
+import { AndroidShortcuts } from 'capacitor-android-shortcuts';
+import { SessionService } from './core/state/session.service';
+import { ShortcutItem } from 'capacitor-android-shortcuts/dist/esm/definitions';
+import { ICONS_PACK } from './shared/icons/icons-pack';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   networkStatus: ConnectionStatus = {
     connected: true,
@@ -23,6 +27,7 @@ export class AppComponent implements OnInit {
     private router: Router,
     private navCtrl: NavController,
     private platform: Platform,
+    private session: SessionService,
   ) {
   }
 
@@ -34,6 +39,8 @@ export class AppComponent implements OnInit {
             Network.getStatus(),
             AppLauncher.canOpenUrl({url: 'br.com.coopersystem.portal'}),
             environment.production ? ScreenOrientation.lock({orientation: 'portrait'}) : ScreenOrientation.unlock(),
+            this.addDynamicShortCuts(),
+            this.listenShortCuts(),
           ]).then((response) => {
             this.networkStatus = response[0];
             console.log('Can open url: ', response[1]);
@@ -49,6 +56,10 @@ export class AppComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    AndroidShortcuts.removeAllListeners().catch();
+  }
+
   navigate(): void {
     if (!this.networkStatus.connected) {
       if (this.router.url !== '/offline') {
@@ -60,6 +71,78 @@ export class AppComponent implements OnInit {
         this.navCtrl.navigateRoot(['/home']).catch();
         return;
       }
+    }
+  }
+
+  private async addDynamicShortCuts(): Promise<void> {
+    const {result} = await AndroidShortcuts.isDynamicSupported();
+    if (result) {
+      const shortCuts: ShortcutItem[] = [
+        {
+          id: 'ponto',
+          shortLabel: 'Bater Ponto',
+          longLabel: 'Registrar ponto',
+          icon: {
+            type: 'Bitmap',
+            name: ICONS_PACK.TICKET,
+          },
+          data: 'ponto',
+        },
+        {
+          id: 'producao',
+          shortLabel: 'Minha produção',
+          longLabel: 'Minha produção',
+          icon: {
+            type: 'Bitmap',
+            name: ICONS_PACK.CALENDAR,
+          },
+          data: 'producao',
+        },
+        {
+          id: 'historico',
+          shortLabel: 'Histórico(GPS)',
+          longLabel: 'Histórico(GPS)',
+          icon: {
+            type: 'Bitmap',
+            name: ICONS_PACK.GPS,
+          },
+          data: 'historico',
+        },
+        {
+          id: 'ferias-abonos',
+          shortLabel: 'Férias e abonos',
+          longLabel: 'Férias e abonos',
+          icon: {
+            type: 'Bitmap',
+            name: ICONS_PACK.BEACH,
+          },
+          data: 'ferias-abonos',
+        },
+        {
+          id: 'perfil',
+          shortLabel: 'Perfil',
+          longLabel: 'Perfil',
+          icon: {
+            type: 'Bitmap',
+            name: ICONS_PACK.PERSON,
+          },
+          data: 'profile',
+        },
+      ];
+      await AndroidShortcuts.setDynamic({
+        items: shortCuts,
+      });
+    }
+    return Promise.resolve();
+  }
+
+  private async listenShortCuts() {
+    const dynamicSupported = await AndroidShortcuts.isDynamicSupported();
+    const pinnedSupported = await AndroidShortcuts.isPinnedSupported();
+    if (dynamicSupported.result || pinnedSupported.result) {
+      await AndroidShortcuts.addListener('shortcut', async listen => {
+        this.router.navigate([`${listen.data}`]).catch();
+      });
     }
   }
 }
